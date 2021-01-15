0992a6bce35133d5452d4d7259487a68a6f8d883
Ellie Lambert <ebezjian@gmail.com>
2021-01-15 15:18:46 -0500

    Add Create to Flavors Controller

--- Bakery/Controllers/FlavorsControllers.cs
+++ Bakery/Controllers/FlavorsControllers.cs
@@ -18,5 +18,16 @@ namespace Bakery.Controllers
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
